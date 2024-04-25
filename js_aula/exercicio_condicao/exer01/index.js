066cf2f4f10238a91c9c769472eb5b09fdc13442
Natalia Fracasso <nataliadiazf05@gmail.com>
2024-04-24 22:45:36 -0300

    exercício de condição

--- js_aula/exercicio_condicao/exer01/index.js
+++ js_aula/exercicio_condicao/exer01/index.js
@@ -1,3 +1,36 @@
+function textoNaTela(tag, text){ // vai receber uma tag e um texto
+    let texto = document.querySelector(tag); // vai selecionar a tag no html
+    texto.innerHTML= text; // a variavel vai receber um texto
+}
 
+textoNaTela('h1', 'Hora do Dia');
 
+function formatarNumero(numero){
+    return numero < 10 ? "0" + numero : numero; 
+}
+
+function mudarCordeFundo(cor){ //Recebe um parâmetro cor
+    document.body.style.backgroundColor = cor; //para definir a cor de fundo do body como a cor passada como parâmetro.
+}
+
+function carregar(){
+    let data = new Date(); // Construtor que contem a data e hora
+    let hora = data.getHours(); // método do new date, que vai retornar a hora. 
+    let min = data.getMinutes(); //método do new date, que vai retornar a min.
+    let img = document.querySelector('#img')
+    textoNaTela('p', `Agora são ${hora} horas e ${min}minutos`)
+
+    if ( hora > 0 && hora < 12){
+        img.src = 'img/dia.jpg';
+
+    } else if (hora > 12 && hora < 18){
+        img.src ='img/tarde.jpg';
+        mudarCordeFundo( 'var(--cor-laranja)' );
+    } else{
+        img.src ='img/noite.jpg';
+        mudarCordeFundo('var(--cor-preto)');
+        
+    }
+
+}
 
