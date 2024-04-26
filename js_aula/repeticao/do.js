7d7e825bea5f2ef8231888d27c8de17adfb32ab3
Natalia Fracasso <nataliadiazf05@gmail.com>
2024-04-25 22:34:10 -0300

    repetiçao

---
+++ js_aula/repeticao/do.js
@@ -0,0 +1,8 @@
+var contador = 1; //O contador começa com a primeira vez
+
+do{ //Faz, enquanto o teste lógico consta como verdadeiro
+
+    console.log(`Passo ${contador}`);
+    contador++ // contador = contador +1 (contador recebe contador, mais um)
+
+}while( contador <= 6); //Enquanto for verdadeiro, repete
---
+++ js_aula/repeticao/while.js
@@ -0,0 +1,7 @@
+var contador = 1; //O contador começa com a primeira vez
+
+while( contador <= 6){ //Enquanto c for menor que 6, vai repetir o texto. 
+
+    console.log(`Passo ${contador}`);
+    contador++ // contador = contador +1 (contador recebe contador, mais um)
+}
