7a406aa0d33da348dd2ab306196c7dfa86d53846
Natalia Fracasso <nataliadiazf05@gmail.com>
2024-05-03 23:07:45 -0300

    function

---
+++ js_aula/funcao_parametro/parametro03/index.js
@@ -0,0 +1,5 @@
+function soma ( n1 = 0, n2 = 0){ // Se na chamda, não for passado os números, cada parâmetro da fucntion vai receber 0, ou outro valor a ser definido.
+    return n1 + n2
+}
+
+console.log(soma(5, 4)); // Chamo a function soma e mostro no console
