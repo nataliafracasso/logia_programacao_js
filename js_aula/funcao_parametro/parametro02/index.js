027003f733ed613607c159740d112e656872c9ba
Natalia Fracasso <nataliadiazf05@gmail.com>
2024-05-03 22:52:41 -0300

    function com parâmetro

---
+++ js_aula/funcao_parametro/parametro02/index.js
@@ -0,0 +1,11 @@
+res = 11;
+
+function parimpar(n){
+    if( res % 2 == 0){
+        console.log('par');
+    } else{
+        console.log('impar');
+    }
+}
+
+parimpar(res); // A variável res q tem o valor 8, está passando p o parâmetro(n), da function parimpar(n).
