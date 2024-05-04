c150f9af19bf4ca0933abdca1bf1f720d24c0094
Natalia Fracasso <nataliadiazf05@gmail.com>
2024-05-03 22:19:19 -0300

    variáveis composta

--- js_aula/variaveis_composta/array.js
+++ js_aula/variaveis_composta/array.js
@@ -1,2 +1,14 @@
-let num = [1, 6, 20, 9]; //Uma variável composta 
- console.log(num)
+let valores = [ 1, 8, 2, 9 ]; //Uma variável composta 
+valores.push(5) // Adiciona o número 5 a variável composta
+console.log(valores.sort()); // ordena os elementos em ordem crescente
+console.log(`A variável valores tem ${valores.length} elementos`)
+for( let contador in valores){ //Para cada posição dentro do da array, vai executar o bloco
+    console.log(`O índice  ${contador}  tem o valor ${valores[contador]}`);
+    
+}
+  
+
+/*for(contador = 0; contador < valores.length; contador++){
+  console.log(`A posição ${contador}  tem o valor ${valores[contador]}`);
+ //Mostra a posição dos valores (Outra forma de executar)
+} */
