08748d3ff6a8f634f9ab49162c510aeed38fbd79
Natalia Fracasso <nataliadiazf05@gmail.com>
2024-05-05 22:35:25 -0300

    somando os valores

--- js_aula/exercicio_function/exer01/index.js
+++ js_aula/exercicio_function/exer01/index.js
@@ -22,12 +22,14 @@ function verificaNumeroLista( n, lista){ // Verifica se o número esta na lista 
         return false
     }
 }
-
+ 
 function adicionarNumero( ){ // Chama as duas functions de verificações e começa a adiconar valores na lista
     
     let numero = Number(document.querySelector('#caixa_numero').value);
     let res = document.querySelector('#res');
 
+    
+    
     if (verificaNumero(numero) && ! verificaNumeroLista(numero, valores)){ // Se o número estiver entre 1 e 100 e NÃO(!) estiver na lista, o numero vai ser adicionado a valores[]
         valores.push(numero); // Vai adicionar no array valores
         let mensagem = '';
@@ -37,15 +39,40 @@ function adicionarNumero( ){ // Chama as duas functions de verificações e come
 
         })
         res.innerHTML = mensagem;
-        document.querySelector('#botao_verifica').style.display = 'block';    
+        document.querySelector('#botao_verifica').style.display = 'block'; // botão de verificar
     } else {
         alert( 'Valor invalido ou já se encontra na lista')
     }
     document.querySelector('#caixa_numero'). value = ''; //limpa a caixa do número    
+   
 }
 
 function analisaNumero(){
-    res.innerHTML = ''; // "Apaga" os valores adicionados p aparecer outro resultado
-    res.innerHTML = `Ao todo, tem ${valores.length} números cadastrados`;
-    res.innerHTML = ` A soma total é ${numero + valores.push(numero)}`;
-}
+    
+    let maiorNumero = Math.max(...valores); // Verifica cada um dos elemetos e mostra o maior número
+    let menorNumero = Math.min(...valores); // Verifica cada um dos elemetos e mostra o menor número
+
+    let soma = 0;
+
+    for (let i = 0; i < valores.length; i++){
+        soma += valores[i] // Ta "sendo o índice de cada elemento", assim pega gada elemento e soma 
+    }
+
+    // Concatena as mensagens em uma única string
+    let mensagemVerifica = `Valores verificados: ${valores} <br>`;
+    mensagemVerifica += `Ao todo, tem ${valores.length} números cadastrados <br>`;
+    mensagemVerifica += `O maior número é ${maiorNumero} <br>`;
+    mensagemVerifica +=` O menor número é ${menorNumero} <br>`;
+    mensagemVerifica += `A soma entre os números é ${soma}`;
+
+    document.querySelector('#res').innerHTML = mensagemVerifica;
+    
+
+    document.querySelector('#botao_verifica').style.display = 'none'; // Após fazer a verificação "some" o botão verificar
+    valores = [];// Limpa a array para guardar os novos valores 
+    
+    
+}  
+
+
+
