2ca619ac19c51f3abe3e26f5d66c4b9ff78ec23f
Natalia Fracasso <nataliadiazf05@gmail.com>
2024-05-22 20:42:49 -0300

    inicio do projeto

---
+++ desafios_alura/logica_programacao/compra_ingressos/js/app.js
@@ -0,0 +1,17 @@
+function comprar(){
+
+    //Pegar valores
+    let escolhaIngresso = document.getElementById('tipo-ingresso').value;
+   
+
+}
+
+//Pegar valores
+  // Tipo de inggresso
+  //quantidade
+  //Tipos de ingressos disponíveis 
+ 
+//Calcular
+    //Subtarair a quantide de comprar de ingresso com os ingressos disponíveis 
+    //Aparecer alert quando a pessoa tentar comprar e não tiver mais ingresso = 0
+
