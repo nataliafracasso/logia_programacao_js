f77099851946e82de7912685ab2a101fadabf6d5
Natalia Fracasso <nataliadiazf05@gmail.com>
2024-06-05 21:25:21 -0300

    alerta ao campo vazio

--- desafios_alura/logica_programacao/amigo-secreto/js/app.js
+++ desafios_alura/logica_programacao/amigo-secreto/js/app.js
@@ -3,8 +3,13 @@ let nomesParaSortear = [];
 function adicionar(){
     let nome = document.getElementById('nome-amigo').value;
     let listaDeAmigos = document.getElementById('lista-amigos');
-    nomesParaSortear.push(nome);
-    listaDeAmigos.innerHTML = nomesParaSortear;
+    
+    if(nome == ''){
+        alert('Digite os nomes');
+    }else{
+        nomesParaSortear.push(nome);
+        listaDeAmigos.innerHTML = nomesParaSortear;
+    }
     limpar();
 }
 
@@ -14,14 +19,15 @@ function sortear(){ // amigo sortear o próximo
     let sortearAmigo = document.getElementById('lista-sorteio');
 
     //O i precisa ser menor que o índice do array 
-    for(i = 0; i < nomesParaSortear.length; i++){ 
+    for(i = 0; i < nomesParaSortear.length; i++){ // Vai repetir enquanto i for menor que os elementos do array
 
-        if( i == nomesParaSortear.length -1){
+        if( i == nomesParaSortear.length -1){ //Se i for igual ao último índice(nome)
 
+            //Pegar o último nome e "juntar" com o primeiro índice[0]
             sortearAmigo.innerHTML = sortearAmigo.innerHTML + nomesParaSortear[i] + '-->' + nomesParaSortear[0] + '<br>';
 
         } else{
-
+            //Se não, junta com o próximo nome índice(lembrando que os nomes foram embaralhados)
             sortearAmigo.innerHTML = sortearAmigo.innerHTML + nomesParaSortear[i] + '-->' + nomesParaSortear[ i + 1] + '<br>';
         }
       
